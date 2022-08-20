f2b59f1dcb6ead6711fcc1d8bdec71f98eb3b6c9
abuzar-gifari <shaikh35-3024@diu.edu.bd>
2022-08-20 13:57:58 +0600

    invoice created

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -9,7 +9,7 @@
 <div class="row">
 <div class="col-12">
 <div class="page-title-box d-sm-flex align-items-center justify-content-between">
-    <h3 class="mb-sm-0" style="font-weight: 900">Dashboard</h3>
+    <h2 class="mb-sm-0" style="font-family: 'Gilroy-Bold', sans-serif;">Invoice Management System Dashboard</h2>
 
     <div class="page-title-right">
         <ol class="breadcrumb m-0">
