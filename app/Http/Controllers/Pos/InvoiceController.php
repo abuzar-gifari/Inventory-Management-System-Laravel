2294285cbfa5bc6cfed5b058ac96b708347b6141
abuzar-gifari <shaikh35-3024@diu.edu.bd>
2022-08-20 17:08:26 +0600

    invoice approve process is done

--- app/Http/Controllers/Pos/InvoiceController.php
+++ app/Http/Controllers/Pos/InvoiceController.php
@@ -186,6 +186,44 @@ class InvoiceController extends Controller
 
     public function ApprovalStore(Request $request, $id){
 
+        foreach($request->selling_qty as $key => $val){
+            $invoice_details = InvoiceDetail::where('id',$key)->first();
+            $product = Product::where('id',$invoice_details->product_id)->first();
+            if($product->quantity < $request->selling_qty[$key]){
+
+                $notification = array(
+                    'message' => 'Sorry you approve Maximum Value', 
+                    'alert-type' => 'error'
+                );
+                return redirect()->back()->with($notification); 
+
+            }
+        } // End foreach 
+
+        $invoice = Invoice::findOrFail($id);
+        $invoice->updated_by = Auth::user()->id;
+        $invoice->status = '1';
+
+        DB::transaction(function() use($request,$invoice,$id){
+            foreach($request->selling_qty as $key => $val){
+             $invoice_details = InvoiceDetail::where('id',$key)->first();
+
+             $invoice_details->status = '1';
+             $invoice_details->save();
+
+             $product = Product::where('id',$invoice_details->product_id)->first();
+             $product->quantity = ((float)$product->quantity) - ((float)$request->selling_qty[$key]);
+             $product->save();
+            } // end foreach
+
+            $invoice->save();
+        });
+
+        $notification = array(
+            'message' => 'Invoice Approve Successfully', 
+            'alert-type' => 'success'
+        );
+        return redirect()->route('invoice.all')->with($notification);
     }
 
     public function PrintInvoiceList(){
